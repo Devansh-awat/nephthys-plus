ad2ec24200c28ca1f88a2a1b1a14c69b0b0dcc40
Devansh Awatramani <80251412+Devansh-awat@users.noreply.github.com>
2026-09-24 20:53:10 +0530

    fix aoishik's dumb mistakes

--- nephthys/utils/slack.py
+++ nephthys/utils/slack.py
@@ -19,6 +19,10 @@ from nephthys.actions.create_category_tag import create_category_tag_btn_callbac
 from nephthys.actions.create_category_tag import create_category_tag_view_callback
 from nephthys.actions.create_team_tag import create_team_tag_btn_callback
 from nephthys.actions.create_team_tag import create_team_tag_view_callback
+from nephthys.actions.manage_macros import delete_macro_callback
+from nephthys.actions.manage_macros import macro_form_view_callback
+from nephthys.actions.manage_macros import open_add_macro_modal
+from nephthys.actions.manage_macros import open_edit_macro_modal
 from nephthys.actions.reopen import reopen
 from nephthys.actions.resolve import resolve
 from nephthys.actions.tag_subscribe import tag_subscribe_callback
@@ -300,3 +304,23 @@ async def submit_feedback(ack: AsyncAck, body: Dict[str, Any], client: AsyncWebC
         response_action="update",
         view=success_modal.build(),
     )
+
+
+@app.action("add-macro")
+async def add_macro(ack: AsyncAck, body: Dict[str, Any], client: AsyncWebClient):
+    await open_add_macro_modal(ack, body, client)
+
+
+@app.action("edit-macro")
+async def edit_macro(ack: AsyncAck, body: Dict[str, Any], client: AsyncWebClient):
+    await open_edit_macro_modal(ack, body, client)
+
+
+@app.action("delete-macro")
+async def delete_macro(ack: AsyncAck, body: Dict[str, Any], client: AsyncWebClient):
+    await delete_macro_callback(ack, body, client)
+
+
+@app.view("macro_form")
+async def macro_form(ack: AsyncAck, body: Dict[str, Any], client: AsyncWebClient):
+    await macro_form_view_callback(ack, body, client)
